44e640834f8c5c5f848d777dae535c0452c3f02c
Noukyyou <108467843+Noukyyou@users.noreply.github.com>
2023-01-06 15:48:38 +0100

    Add files via upload

--- color toggle/script.js
+++ color toggle/script.js
@@ -1,16 +1,73 @@
-const navToggleStatus = false;
+let navToggleStatus = false;
 
-const menuBtn = document.getElementById('menu');
-const getSideBar = document.getElementsByClassName('.nav-sidebar');
-const getSidebarUL =document.querySelector ('.nav-sidebar ul')
-const getSidebarLinks=document.querySelectorAll ('.nav-sidebar li)');
+let menuBtn = document.getElementById('menu');
+let getSideBar = document.getElementsByClassName('.nav-sidebar');
+let getSidebarUL =document.querySelector ('.nav-sidebar ul')
+let getSidebarLinks=document.querySelectorAll ('.nav-sidebar ul li');
 
 console.log(menuBtn);
 console.log(getSideBar);
 
 menuBtn.addEventListener ('click', function(){
   console.log('menu is clicked');
+
   if (navToggleStatus === false) {
     getSidebarUL.style.visibility = 'visible';
+    //getSidebarLinks.style.width ='300px';
+  
+  let arrayLenght = getSidebarLinks.length;
+  for (let i=0; i < arrayLenght; i++) {
+    getSidebarLinks[i].style.opacity = "100";
   }
+
+  navToggleStatus = true;
+}
+  
+  else if (navToggleStatus === true) {
+      //getSidebarLinks.style.width ='0px';
+  
+  let arrayLenght = getSidebarLinks.length;
+  for (let i=0; i < arrayLenght; i++) {
+    getSidebarLinks[i].style.opacity = "0";
+  }
+    getSidebarUL.style.visibility = "hidden";
+    navToggleStatus = false;
+  }
+  
+})
+
+const homebg = document.getElementById('home');
+  homebg.addEventListener("click", function(){
+  document.body.style.backgroundColor = 'lightgrey';
+  console.log('Home is clicked');
+  getSidebarUL.style.visibility = 'hidden';
+  })
+
+const redbg = document.getElementById('red');
+  redbg.addEventListener("click", function(){
+  document.body.style.backgroundColor = 'red';
+  console.log('Red is clicked');
+  getSidebarUL.style.visibility = 'hidden';
+})
+
+
+const orangebg = document.getElementById('orange');
+  orangebg.addEventListener("click", function(){
+  document.body.style.backgroundColor = 'orange';
+  console.log('Orange is clicked');
+  getSidebarUL.style.visibility = 'hidden';
+})
+
+const purplebg = document.getElementById('purple');
+  purplebg.addEventListener("click", function(){
+  document.body.style.backgroundColor = 'purple';
+  console.log('Purple is clicked');
+  getSidebarUL.style.visibility = 'hidden';
+})
+
+const greenbg = document.getElementById('green');
+  greenbg.addEventListener("click", function(){
+  document.body.style.backgroundColor = 'green';
+  console.log('Green is clicked');
+  getSidebarUL.style.visibility = 'hidden';
 })
